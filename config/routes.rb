c7e4583427c095ac42b374651bfa0e7a9105b10c
Guillaume Genet <wagui.genet@gmail.com>
2022-07-19 19:14:13 +0200

    setup routes services

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :services
 end
