487c2ac0ccae297eba35e08fa90d04255c1661c5
Guillaume Genet <wagui.genet@gmail.com>
2022-07-31 18:53:21 +0200

    remise a jour setup shema

--- db/schema.rb
+++ db/schema.rb
@@ -10,32 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_19_155202) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_31_163800) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'partenaires', force: :cascade do |t|
-    t.string 'name'
-    t.string 'address'
-    t.string 'website'
-    t.string 'logo'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "partenaires", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "website"
+    t.string "logo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'services', force: :cascade do |t|
-    t.string 'name'
-    t.text 'description'
-    t.integer 'price_1'
-    t.integer 'price_2'
-    t.integer 'price_3'
-    t.string 'photo_principal'
-    t.string 'photo_2'
-    t.string 'photo_3'
-    t.string 'photo_4'
-    t.string 'photo_5'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "services", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "photo_principal"
+    t.string "photo_2"
+    t.string "photo_3"
+    t.string "photo_4"
+    t.string "photo_5"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "Captation"
+    t.float "Traitement"
+    t.float "Déplacement"
   end
 
 end
